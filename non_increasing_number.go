fda61b442caf1d97f42b749166ffea5473cc9ac1
jonas <49788305+JonasMuehlmann@users.noreply.github.com>
2024-06-04 09:25:15 +0200

    refactor!: NonIncreasingInt -> NonIncreasingNumber

---
+++ non_increasing_number.go
@@ -0,0 +1,29 @@
+package main
+
+type NonIncreasingNumber[T Number] struct {
+	ValidatedNumber[T]
+}
+
+func NewNonIncreasingNumber[T Number](value T) NonIncreasingNumber[T] {
+	object := NonIncreasingNumber[T]{}
+
+	object.value = value
+
+	return object
+}
+
+func NewNonIncreasingNumberBare[T Number]() NonIncreasingNumber[T] {
+	object := NonIncreasingNumber[T]{}
+
+	return object
+}
+
+func (v *NonIncreasingNumber[T]) Set(value T) {
+	if DEBUG {
+		if value <= v.value {
+			panic("Value must be nonincreasing")
+		}
+	}
+
+	v.ValidatedNumber.Set(value)
+}
