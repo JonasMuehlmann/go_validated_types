70e0f11a5fdbef1239b16b1e4697344dc26db480
jonas <49788305+JonasMuehlmann@users.noreply.github.com>
2024-06-04 09:12:21 +0200

    feat: add PrefixString

---
+++ prefix_string.go
@@ -0,0 +1,17 @@
+package main
+
+type PrefixString struct {
+	ValidatedString
+}
+
+func NewPrefixString(value string, prefix string) PrefixString {
+	object := PrefixString{}
+	object.value = value
+
+	if DEBUG {
+		object.validator = MakePrefixValidator(prefix)
+		object.validate()
+	}
+
+	return object
+}
