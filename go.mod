7b20d95d511493b375ab89fb78995769fef7097c
jonas <49788305+JonasMuehlmann@users.noreply.github.com>
2024-06-04 09:01:25 +0200

    feat: add ValidatedString

--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module validated_types
 
 go 1.22
 
-require github.com/stretchr/testify v1.9.0
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require golang.org/x/exp v0.0.0-20240531132922-fd00a4e0eefc
---
+++ validated_string.go
@@ -0,0 +1,50 @@
+package main
+
+type ValidatedString struct {
+	ValidatedType[string]
+	value     string
+	validator Validator[string]
+}
+
+func (v *ValidatedString) validate() {
+	if v.validator != nil {
+		v.validator(v.value)
+	}
+}
+
+func (v *ValidatedString) Get() string {
+	return v.value
+}
+
+func (v *ValidatedString) Set(value string) {
+	v.value = value
+	if DEBUG {
+		v.validate()
+	}
+}
+
+func NewValidatedString(value string, validator Validator[string]) ValidatedString {
+	object := ValidatedString{value: value}
+
+	if DEBUG {
+		object.validator = validator
+		if object.validator != nil {
+			object.validate()
+		}
+	}
+
+	return object
+}
+
+func NewValidatedStringBare(validator Validator[string]) ValidatedString {
+	object := ValidatedString{}
+
+	if DEBUG {
+		object.validator = validator
+		if object.validator != nil {
+			object.validate()
+		}
+	}
+
+	return object
+}
