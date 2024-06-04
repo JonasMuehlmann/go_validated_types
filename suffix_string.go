d2127fcb5d37e27dafb7244643cc3801eea55701
jonas <49788305+JonasMuehlmann@users.noreply.github.com>
2024-06-04 09:12:40 +0200

    feat: add SuffixString

---
+++ suffix_string.go
@@ -0,0 +1,17 @@
+package main
+
+type SuffixString struct {
+	ValidatedString
+}
+
+func NewSuffixString(value string, suffix string) SuffixString {
+	object := SuffixString{}
+	object.value = value
+
+	if DEBUG {
+		object.validator = MakeSuffixValidator(suffix)
+		object.validate()
+	}
+
+	return object
+}
